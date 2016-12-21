2a8a5ffabcf319a72f61297388bc5903088217bc
15529343201 <15529342101@139.com>
2016-12-21 22:43:30 +0800

    add Exercise7_1.java

---
+++ javaBasicArticles/Chapter7/Exercise7_1.java
@@ -0,0 +1,33 @@
+package chapter7;
+//求矩阵中所有数字之和
+import java.util.Scanner;
+public class Exercise7_1 
+{
+	public static void main(String[] args)
+	{
+		Scanner input = new Scanner(System.in);
+		
+		System.out.println("Enter a 4-by-4 matrix row by row:");
+		double[][] m = new double[4][4];
+		
+		for(int i = 0; i < 4; i++)
+			for(int j =0; j < 4; j++)
+				m[i][j] = input.nextDouble();
+		
+		System.out.println("Sum of the martix is " + sumMatrix(m));
+		input.close();
+	}
+	
+	public static double sumMatrix(double[][] m)
+	{
+		double sum = 0;
+		for(int i = 0; i < m.length; i++)
+		{
+			for(int j = 0; j < m[0].length; j++)
+			{
+				sum += m[i][j];
+			}
+		}
+		return sum;
+	}
+}
